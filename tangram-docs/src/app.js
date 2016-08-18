38d3aac76acbbaf81584920becdc8db0d26cc214
Irene Alvarado <ire.alvarado@gmail.com>
2016-08-18 13:29:55 -0400

    Cleaning up node app.js file

--- tangram-docs/src/app.js
+++ tangram-docs/src/app.js
@@ -14,90 +14,17 @@ var object = {
     parameter: []
 }
 
-// function flattenDocs () {
-//     walker.on('file', fileHandler);
-//     walker.on('end', endHandler);
-// }
-
-/* Iterate through entire directory */
-function fileHandler (root, fileStat, next) {
-    let dir_file = path.resolve(root, fileStat.name);
-    let check = 'documentation/parameters/';
-    let rel_path = dir_file.substring(dir_file.indexOf(check) + check.length);
-    rel_path = rel_path.substring(0, rel_path.length - 5);
-    let split = rel_path.split('/');
-
-    fs.readFile(dir_file, 'ascii', function (err, data) {
-        if (endsWith(fileStat.name, '.yaml')) {
-            var doc = yaml.safeLoad(data);
-            addToMainObject(split, doc.parameter);
-        }
-
-        next();
-    });
+function flattenDocs () {
+    console.log("Adding first level nodes to the tree...", "\n");
+    walker.on('file', fileHandler);
+    walker.on('end', endHandler);
 }
 
-function endHandler () {
-    writeJSON();
-}
-
-function endsWith (str, suffix) {
+function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-function addToMainObject (split, nodeToAdd) {
-    console.log("***********addding object***********");
-    let level = split[0];
-    let currentTreeLevel = object.parameters;
-
-    // If the first level of the tree
-    if (split.length === 1) {
-        object.parameters.push(nodeToAdd);
-    }
-    // For all other levels we have to iterate through tree to find where to insert the node
-    else {
-        for (let i = 1; i < split.length; i++) {
-            for (let node of currentTreeLevel) {
-                // console.log(currentTreeLevel);
-                if (node.name = split[i]) {
-                    console.log(i);
-                    console.log("match between", node.name, split[i]);
-                    console.log(node);
-                    console.log("\n\n");
-                    currentTreeLevel = node;
-                    break;
-                    // console.log(node);
-                    // console.log("\n\n");
-                }
-            }
-        }
-
-        currentTreeLevel.children.push(nodeToAdd);
-    }
-}
-
-function writeJSON () {
-    console.log("writing to JSON");
-    fs.writeFile('tangram.json', JSON.stringify(object, null, 2), 'utf8', (err) => {
-        if (err) {
-            throw err;
-        }
-        else {
-            console.log('JSON doc saved!');
-        }
-    });
-}
-
-// flattenDocs();
-
-/************/
-
-function flattenDocs2 () {
-    walker.on('file', fileHandler2);
-    walker.on('end', endHandler2);
-}
-
-function fileHandler2 (root, fileStat, next) {
+function fileHandler (root, fileStat, next) {
     let dir_file = path.resolve(root, fileStat.name);
     let check = 'documentation/parameters/';
     let rel_path = dir_file.substring(dir_file.indexOf(check) + check.length);
@@ -110,32 +37,23 @@ function fileHandler2 (root, fileStat, next) {
             // Add only the first level of the tree (level 1, i.e. sources, camera, globals, etc.
             if (split.length === 1) {
                 let firstNode = yaml.safeLoad(data);
-                // console.log(firstNode);
                 object.parameter.push(firstNode.parameter);
             }
-
-            // console.log(split);
-            // addToMainObject(split, doc.parameter);
         }
 
         next();
     });
 }
 
-function endHandler2 () {
-    console.log("Adding children to the tree...");
-    // console.log(object.parameter[1]);
-
+function endHandler () {
     let first_path = '../../documentation/parameters/';
 
+    console.log("Recursively iterating through the tree...", "\n")
     for (let i = 0; i < object.parameter.length; i++) {
         object.parameter[i] = recursiveAdd(object.parameter[i], first_path);
     }
 
-    // object.parameter = recursiveAdd(object.parameter[1], first_path);
-    console.log("printing my object");
-    console.log(object.parameter);
-    writeJSON2(object);
+    writeJSON(object);
 }
 
 
@@ -162,9 +80,8 @@ function recursiveAdd (node, base_path) {
         }
 }
 
-function writeJSON2 () {
-    console.log("writing to JSON");
-    fs.writeFile('tangram.json', JSON.stringify(object, null, 4), 'utf8', (err) => {
+function writeJSON () {
+    fs.writeFile('../dist/tangram-docs.json', JSON.stringify(object, null, 4), 'utf8', (err) => {
         if (err) {
             throw err;
         }
@@ -174,4 +91,4 @@ function writeJSON2 () {
     });
 }
 
-flattenDocs2();
+flattenDocs();
